6a15678062b6659129b908ac9b1eda0bfde3e49a
6baobao9 <wangyu.0619@qq.com>
2018-11-01 08:07:34 +0800

    spring定时任务demo

--- src/main/java/com/wy/springtest/schedule/TestTask.java
+++ src/main/java/com/wy/springtest/schedule/TestTask.java
@@ -1,4 +1,22 @@
 package com.wy.springtest.schedule;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * 定时任务demo
+ */
+@Configuration
+// 开启spring定时任务
+@EnableScheduling
 public class TestTask {
+    private static Log logger = LogFactory.getLog(TestTask.class);
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void schedule1() {
+        logger.info("定时任务：每五秒执行一次");
+    }
 }
