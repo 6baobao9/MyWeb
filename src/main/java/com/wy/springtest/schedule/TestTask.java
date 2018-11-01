ac630bdb5675b1df4d2a0dc5ddc6373920d17f20
6baobao9 <wangyu.0619@qq.com>
2018-11-01 08:17:56 +0800

    spring定时任务demo

--- src/main/java/com/wy/springtest/schedule/TestTask.java
+++ src/main/java/com/wy/springtest/schedule/TestTask.java
@@ -15,8 +15,25 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class TestTask {
     private static Log logger = LogFactory.getLog(TestTask.class);
 
-    @Scheduled(cron = "0/5 * * * * ?")
+    @Scheduled(cron = "0 * * * * ?")
     public void schedule1() {
-        logger.info("定时任务：每五秒执行一次");
+        logger.info("定时任务：每到0秒执行一次");
     }
+
+    @Scheduled(fixedDelay = 5000)
+    public void schedule2() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        logger.info("定时任务：每执行后等5秒再执行");
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void schedule3() {
+        logger.info("定时任务：每5秒执行一次");
+    }
+
+
 }
