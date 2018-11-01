4700251d704ae551784a8d9375b6c9dea77d4ebb
6baobao9 <wangyu.0619@qq.com>
2018-11-01 08:29:53 +0800

    spring跳转控制器

--- src/main/java/com/wy/springtest/controller/HelloController.java
+++ src/main/java/com/wy/springtest/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * spring跳转控制器，通过浏览器访问路径进入方法
+ */
 @RestController
 @RequestMapping(path = "/hello")
 public class HelloController {
