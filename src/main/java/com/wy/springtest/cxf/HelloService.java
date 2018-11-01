8e159ac838194504653401568cf7427837aad420
王宇 <wangyu.0619@qq.com>
2018-11-01 16:26:07 +0800

    使用CXF实现webservice

--- src/main/java/com/wy/springtest/cxf/HelloService.java
+++ src/main/java/com/wy/springtest/cxf/HelloService.java
@@ -1,5 +1,6 @@
 package com.wy.springtest.cxf;
 
+import com.wy.springtest.cxf.model.User;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
@@ -8,10 +9,16 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @Component(value = "helloService")
-@WebService(name = "helloService")
+// 修改接口命名空间
+@WebService(name = "helloService", targetNamespace = "http://service.wy.com")
 public class HelloService {
     @WebMethod
     public String say(@WebParam(name = "name") String name) {
         return "Hello " + name;
+    }
+
+    @WebMethod
+    public String sayToUser(@WebParam(name = "user") User user) {
+        return "Hello " + user.getName();
     }
 }
