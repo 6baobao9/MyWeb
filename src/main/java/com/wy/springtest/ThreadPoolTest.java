18abdc95e21819ec6d3f474e935586e82c4b1093
6baobao9 <wangyu.0619@qq.com>
2018-11-23 21:25:13 +0800

    20181123

--- src/main/java/com/wy/springtest/ThreadPoolTest.java
+++ src/main/java/com/wy/springtest/ThreadPoolTest.java
@@ -21,7 +21,7 @@ public class ThreadPoolTest implements InitializingBean {
         ThreadPoolExecutor.AbortPolicy:丢弃任务并抛出RejectedExecutionException异常。
         ThreadPoolExecutor.DiscardPolicy：也是丢弃任务，但是不抛出异常。
         ThreadPoolExecutor.DiscardOldestPolicy：丢弃队列最前面的任务，然后重新尝试执行任务（重复此过程）
-        ThreadPoolExecutor.CallerRunsPolicy：由调用线程处理该任务
+     jv   ThreadPoolExecutor.CallerRunsPolicy：由调用线程处理该任务
         */
         threadPoolExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
     }
--- src/main/java/com/wy/springtest/example/InstantiationAwareBeanPostProcessorImpl.java
+++ src/main/java/com/wy/springtest/example/InstantiationAwareBeanPostProcessorImpl.java
@@ -4,32 +4,34 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
-import org.springframework.stereotype.Component;
 
-@Component
 public class InstantiationAwareBeanPostProcessorImpl extends InstantiationAwareBeanPostProcessorAdapter implements InstantiationAwareBeanPostProcessor {
-
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        // 执行顺序：5
         System.out.println("Enter InstantiationAwareBeanPostProcessorBean.postProcessAfterInitialization()");
         return bean;
     }
 
     @Override
     public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+        // 执行顺序：3
         return super.postProcessProperties(pvs, bean, beanName);
     }
 
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        // 执行顺序：4
         System.out.println("Enter InstantiationAwareBeanPostProcessorBean.postProcessBeforeInitialization()");
         return bean;
     }
 
     public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+        // 执行顺序：2
         System.out.println("Enter InstantiationAwareBeanPostProcessorBean.postProcessAfterInstantiation()");
         return true;
     }
 
     public Object postProcessBeforeInstantiation(Class<?> bean, String beanName) throws BeansException {
+        // 执行顺序：1
         System.out.println("Enter InstantiationAwareBeanPostProcessorBean.postProcessBeforeInstantiation()");
         return null;
     }
